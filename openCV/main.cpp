325b46dd8c8b88297cd985d0a972fe85682db1e6
VivienZhou <tiantong@umich.edu>
2016-06-13 20:41:05 +0800

    Initial Commit

---
+++ openCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openCV
+//
+//  Created by Tiantong Zhou on 6/13/16.
+//  Copyright © 2016 Tiantong Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
