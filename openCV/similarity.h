8584871c7ecf62b5c8f33b0be3abc14fcc9f0675
darlenelyj <luyoujia@umich.edu>
2016-06-21 23:44:46 +0800

    finish implementing similarity metrics

---
+++ openCV/similarity.h
@@ -0,0 +1,13 @@
+//
+//  similarity.h
+//  openCV
+//
+//  Created by 鲁又佳 on 6/21/16.
+//  Copyright (c) 2016 Tiantong Zhou. All rights reserved.
+//
+
+#ifndef openCV_similarity_h
+#define openCV_similarity_h
+
+
+#endif
