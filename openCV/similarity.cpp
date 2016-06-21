fdb65df68987d3997fb11b813fcb0211fbf6ebdf
darlenelyj <luyoujia@umich.edu>
2016-06-21 23:46:33 +0800

    finish implementing similarity metrics, modulize the structure

--- openCV/similarity.cpp
+++ openCV/similarity.cpp
@@ -1,4 +1,4 @@
-#include "frame.h"
+#include "similarity.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
@@ -9,39 +9,65 @@
 using namespace std;
 using namespace cv;
 
-// Input: length and width of the window, center of the window in the image
-// Output: Matrix of the extracted window
-Mat get_window_mat(int length, int width, Point loc, const Mat& image_mat);
-
 // Given two matrices, get the unnormed cosine similarity of the two matrices
-double correlation(Mat template_mat, Mat img_window_mat);
+double correlation(Mat template_mat, Mat img_window_mat) {
+    double result = template_mat.dot(img_window_mat);
+    return result;
+}
 
 // Given two matrices, get the cosine similarity of the two matrices
-double correlation_normed(Mat template_mat, Mat img_window_mat);
+double correlation_normed(Mat template_mat, Mat img_window_mat) {
+    double correlation_res = correlation(template_mat, img_window_mat);
+    double result = correlation_res / (norm(template_mat) * norm(img_window_mat));
+    return result;
+}
 
 // Given two matrices, get the square difference of the two matrices
-double square_diff(Mat template_mat, Mat img_window_mat);
+double square_diff(Mat template_mat, Mat img_window_mat) {
+    double result_sqrt = norm(template_mat, img_window_mat);
+    return result_sqrt * result_sqrt;
+}
 
 // Given two matrices, get the normed square difference of the two matrices
-double square_diff_normed(Mat template_mat, Mat img_window_mat);
+double square_diff_normed(Mat template_mat, Mat img_window_mat) {
+    double square_diff_res = square_diff(template_mat, img_window_mat);
+    double result = square_diff_res / (norm(template_mat) * norm(img_window_mat));
+    return result;
+}
+
+// Get the matrix of same size where every value is the mean value of the original matrix
+Mat get_mean_mat(Mat input_mat) {
+    double mean_val = sum(input_mat)[0] / (input_mat.rows * input_mat.cols);
+    Mat mean_mat = Mat::ones(input_mat.rows, input_mat.cols, CV_32FC1) * mean_val;
+    return mean_mat;
+}
+
+// Subtract the mean value matrix from the original matrix
+Mat get_deviation_mat(Mat input_mat) {
+    return (input_mat - get_mean_mat(input_mat));
+}
 
 // Input: template matrix and window matrix in the image.
 // For the two given matrices, subtract an average pixel value
 // from each pixel and get two deviation matrices Then do the dot product of the
 // two deviation matrices,
 // output is the dot product of the deviation matrices
-double coefficient(Mat template_mat, Mat img_window_mat);
+double coefficient(Mat template_mat, Mat img_window_mat) {
+    return correlation(get_deviation_mat(template_mat), get_deviation_mat(img_window_mat));
+}
+
 
 // Input: template matrix and window matrix in the image.
 // For the two given matrices, subtract an average pixel value
 // from each pixel and get two deviation matrices Then do the dot product of the
 // two deviation matrices,
 // output is the cosine similarity of the deviation matrices
-double coefficient_normed(Mat template_mat, Mat img_window_mat);
+double coefficient_normed(Mat template_mat, Mat img_window_mat) {
+    double coefficient_res = coefficient(template_mat, img_window_mat);
+    double result = coefficient_res / (norm(get_deviation_mat(template_mat)) * norm(get_deviation_mat(img_window_mat)));
+    return result;
+}
 
-// method_map maps string of method name to function pointer that calculates
-// corresponding similarity score.
-using Method_map_t = map<string, double(*)(Mat, Mat)>;
 Method_map_t method_map = map<string, double(*)(Mat, Mat)> {
     {"correlation", &correlation},
     {"correlation_normed", &correlation_normed},
@@ -51,23 +77,7 @@ Method_map_t method_map = map<string, double(*)(Mat, Mat)> {
     {"coefficient_normed", &coefficient_normed}
 };
 
-// Given method_name and two matrices, calculate the similarity score of the corresponding method
-double similarity_score(string sim_method_name, Mat template_mat, Mat img_window_mat) {
-    return method_map[sim_method_name](template_mat, img_window_mat);
+double get_similarity_score(string method, Mat template_mat, Mat img_window_mat) {
+    return method_map[method](template_mat, img_window_mat);
 }
-
-// Input: template matrix, original image matrix, threshold value
-// Usage: calculate similaritis of all locations
-// Output: a vector of all the matching locations higher than the threshold
-vector<Point> get_locations_sliding_window(Mat template_mat, Mat image_mat, string sim_method_name, double threshold) {
-    vector<Point> matching_locations;
-    // For loops
-    // inside for loop: use get_window_mat, similarity_score
-    return matching_locations;
-}
-
-
-
-
-
 
--- openCV/similarity.h
+++ openCV/similarity.h
@@ -1,13 +1,50 @@
-//
-//  similarity.h
-//  openCV
-//
-//  Created by 鲁又佳 on 6/21/16.
-//  Copyright (c) 2016 Tiantong Zhou. All rights reserved.
-//
+#ifndef SMILARITY_H
+#define SMILARITY_H
 
-#ifndef openCV_similarity_h
-#define openCV_similarity_h
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <map>
+using cv::Mat;
+
+// Given two matrices, get the unnormed cosine similarity of the two matrices
+double correlation(Mat template_mat, Mat img_window_mat);
+
+// Given two matrices, get the cosine similarity of the two matrices
+double correlation_normed(Mat template_mat, Mat img_window_mat);
+
+// Given two matrices, get the square difference of the two matrices
+double square_diff(Mat template_mat, Mat img_window_mat);
+
+// Given two matrices, get the normed square difference of the two matrices
+double square_diff_normed(Mat template_mat, Mat img_window_mat);
+
+// Get the matrix of same size where every value is the mean value of the original matrix
+Mat get_mean_mat(Mat input_mat);
+
+// Subtract the mean value matrix from the original matrix
+Mat get_deviation_mat(Mat input_mat);
+
+// Input: template matrix and window matrix in the image.
+// For the two given matrices, subtract an average pixel value
+// from each pixel and get two deviation matrices Then do the dot product of the
+// two deviation matrices,
+// output is the dot product of the deviation matrices
+double coefficient(Mat template_mat, Mat img_window_mat);
+
+// Input: template matrix and window matrix in the image.
+// For the two given matrices, subtract an average pixel value
+// from each pixel and get two deviation matrices Then do the dot product of the
+// two deviation matrices,
+// output is the cosine similarity of the deviation matrices
+double coefficient_normed(Mat template_mat, Mat img_window_mat);
+
+using Method_map_t = std::map<std::string, double(*)(Mat, Mat)>;
+
+// This is the main function used by client
+// method is one of the followings: "correlation", "correlation_normed", "square_diff", "square_diff_normed", "coefficient", "coefficient_normed"
+// template_mat is the matrix of the template image
+// img_window_mat is the matrix captured from the original image matrix
+double get_similarity_score(std::string method, Mat template_mat, Mat img_window_mat)
 
 
 #endif
