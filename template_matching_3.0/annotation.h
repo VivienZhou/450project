ac033952df51422e873e225ffed2dc779363c4e5
VivienZhou <tiantong@umich.edu>
2016-07-11 17:10:20 +0800

    change annotation

---
+++ template_matching_3.0/annotation.h
@@ -0,0 +1,14 @@
+
+#ifndef ANNOTATION_H
+#define ANNOTATION_H
+
+#include "utility.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <cmath>
+#include <string>
+
+void draw_boundary(const center_and_angle_t& object_info, int scaleCol, int scaleRow, cv::Mat& test_img);
+
+#endif 
