57f9b0bfbcabbbe28aa48d59d9231112581799ed
darlenelyj <luyoujia@umich.edu>
2016-07-18 15:40:27 +0800

    add main.cpp

--- template_matching_4.0/main.cpp
+++ template_matching_4.0/main.cpp
@@ -9,10 +9,14 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "get_location_and_rotation.h"
+#include "resize_and_get_location_and_rotation.h"
 #include "get_multi_location_and_rotation.h"
+#include "get_accurate_center_and_rotation.h"
+#include "crop_img.h"
 #include "annotation.h"
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -20,33 +24,42 @@ using namespace cv;
 int main( int argc, char** argv )
 {
     //read image
+    Mat templ_img = imread("/Users/luyoujia/Documents/study_2016Summer/VE450/template/engine_parts/1_model.png", 0);
+    Mat test_img = imread("/Users/luyoujia/Documents/study_2016Summer/VE450/engine_parts/board/1_image_8.png", 0);
+
     //Mat templ_img = imread(argv[1], 0);
     //Mat test_img = imread(argv[2], 0);
-    Mat templ_img = imread("/Users/luyoujia/Documents/study_2016Summer/VE450/template/engine_parts/1_model.png", 0);
-    Mat test_img = imread("/Users/luyoujia/Documents/study_2016Summer/VE450/template/engine_parts/1_image_8.png", 0);
-
-    cout << "begin single object" << endl;
-    center_and_angle_t result = get_location_and_rotation(templ_img, test_img, 0, 360, 1);
-    cout << "end single object" << endl;
-
-
+    //center_and_angle_t result = resize_and_get_location_and_rotation(templ_img, test_img);
+    double resize_factor = 0.2;
+    Mat resized_test_img, resized_templ_img;
+    resize(test_img, resized_test_img, Size(), resize_factor, resize_factor, INTER_NEAREST);
+    resize(templ_img, resized_templ_img, Size(), resize_factor, resize_factor, INTER_NEAREST);
+    
     double threshold = 0.85;
-    cout << "begin multiple object" << endl;
-    vector<center_and_angle_t> result_vec = get_multi_location_and_rotation(templ_img, test_img, threshold, 0, 360, 5);
-
-
-    cout << result_vec.size() << endl;
-    for (int i = 0; i < result_vec.size(); i ++){
-        cout << "Object " << i + 1 << endl;
-        cout << "angle: " << result_vec[i].angle << endl;
-        cout << "x: " << result_vec[i].center.x << endl;
-        cout << "y: " << result_vec[i].center.y << endl;
+    vector<center_and_angle_t> rough_centers_and_angles = get_multi_location_and_rotation(resized_templ_img, resized_test_img, threshold, 0, 360, 5);
+    
+    int result_num = static_cast<int>(rough_centers_and_angles.size());
+    double crop_size = sqrt(templ_img.cols * templ_img.cols + templ_img.rows * templ_img.rows);
+    
+    // a vector storing final results
+    vector<center_and_angle_t> result_centers_and_angles;
+    
+    for (int i = 0; i < result_num; ++i) {
+        Point upper_left_corner;
+        Mat cropped_img = crop_img(test_img, upper_left_corner, crop_size, rough_centers_and_angles[i].center, resize_factor);
+        center_and_angle_t result = get_accurate_center_and_angle(cropped_img, templ_img, upper_left_corner, rough_centers_and_angles[i].angle);
+        result_centers_and_angles.push_back(result);
     }
-    cout << "end multiple object" << endl;
-
+    
+    // calculate the rotation angle and center location in the resized test image
+    // center_and_angle_t resized_center_and_angle = get_location_and_rotation(resized_templ_img, resized_test_img);
+    
+    
+    // calculate the rotation angle and center location in the resized test image
+    //center_and_angle_t resized_center_and_angle = get_location_and_rotation(resized_templ_img, resized_test_img);
 
     //annotate the image
-    //draw_boundary(result, templ_img.cols, templ_img.rows, test_img);
+    draw_boundary(result_centers_and_angles, templ_img.cols, templ_img.rows, test_img);
 
     //waitKey(0);
     return 0;
